45ab81d5d73303e3f1685153c2df94c2e5fe3341
Pavel Gladkov <gladkov.p@gmail.com>
2020-05-05 18:57:21 +0300

    run

--- arc_run.py
+++ arc_run.py
@@ -5,7 +5,7 @@ import torch
 from tqdm import tqdm
 
 from arc_utils import load_data, flattener, dump_json, get_logger
-from arc_models import TaskSolver, evaluate, input_output_shape_is_same, build_base_net
+from arc_models import TaskSolver, evaluate, input_output_shape_is_same
 import pickle
 
 BASE_PATH = '/data/arc'
@@ -17,8 +17,8 @@ DEBUG = True
 logger = get_logger()
 
 
-def make_prediction(tasks, logger, base_net=None, debug=False):
-    ts = TaskSolver(logger, base_net)
+def make_prediction(tasks, logger):
+    ts = TaskSolver(logger)
     result = pd.Series()
     for idx, task in tqdm(tasks.items()):
         if input_output_shape_is_same(task):
@@ -29,8 +29,6 @@ def make_prediction(tasks, logger, base_net=None, debug=False):
 
         for i, p in enumerate(pred):
             result[f'{idx}_{i}'] = flattener(np.array(p).tolist())
-            if debug:
-                dump_json(f'{idx}_{i}', task['test'][i]['input'], p)
 
     return result
 
@@ -40,16 +38,13 @@ if __name__ == '__main__':
     evaluation_tasks = load_data(BASE_PATH + '/evaluation')
     test_tasks = load_data(BASE_PATH + '/test')
 
-    # base_net = build_base_net(train_tasks, logger)
-    base_net = None
-
-    train_result, train_predictions = evaluate(train_tasks, logger, base_net)
+    train_result, train_predictions = evaluate(train_tasks, logger)
     train_solved = [any(score) for score in train_result]
 
     total = sum([len(score) for score in train_result])
-    logger.info(f"solved : {sum(train_solved)} from {total} ({sum(train_solved) / total})")
+    logger.info(f"train solved : {sum(train_solved)} from {total} ({sum(train_solved) / total})")
 
-    evaluation_result, evaluation_predictions = evaluate(evaluation_tasks, logger, base_net)
+    evaluation_result, evaluation_predictions = evaluate(evaluation_tasks, logger)
     evaluation_solved = [any(score) for score in evaluation_result]
 
     if DEBUG:
@@ -63,9 +58,9 @@ if __name__ == '__main__':
             pickle.dump(evaluation_solved, f)
 
     total = sum([len(score) for score in evaluation_result])
-    logger.info(f"solved : {sum(evaluation_solved)} from {total} ({sum(evaluation_solved) / total})")
+    logger.info(f"evaluation solved : {sum(evaluation_solved)} from {total} ({sum(evaluation_solved) / total})")
 
-    submission = make_prediction(test_tasks, logger, base_net, DEBUG)
+    submission = make_prediction(test_tasks, logger)
     submission = submission.reset_index()
     submission.columns = ['output_id', 'output']
     submission.to_csv('submission.csv', index=False)
