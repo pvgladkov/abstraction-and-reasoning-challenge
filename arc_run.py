27dfb4fea0e63cc195a3022baa62bebcec78322f
Pavel Gladkov <gladkov.p@gmail.com>
2020-05-09 12:49:23 +0300

    trees

--- arc_run.py
+++ arc_run.py
@@ -3,7 +3,8 @@ import pandas as pd
 from tqdm import tqdm
 
 from arc.utils import load_data, flattener, get_logger
-from arc.models import TaskSolverConv1, evaluate, input_output_shape_is_same
+from arc.models import TaskSolverConv1, input_output_shape_is_same, calc_score
+from arc.trees import TaskSolverTree
 import pickle
 
 BASE_PATH = '/data/arc'
@@ -15,13 +16,15 @@ DEBUG = True
 logger = get_logger()
 
 
-def make_prediction(tasks, logger):
-    ts = TaskSolverConv1(logger)
+def make_prediction(tasks, solver):
     result = pd.Series()
     for idx, task in tqdm(tasks.items()):
         if input_output_shape_is_same(task):
-            ts.train(task['train'])
-            pred = ts.predict(task['test'])
+            task_result = solver.train(task['train'])
+            if task_result:
+                pred = solver.predict(task['test'])
+            else:
+                pred = [el['input'] for el in task['test']]
         else:
             pred = [el['input'] for el in task['test']]
 
@@ -31,18 +34,45 @@ def make_prediction(tasks, logger):
     return result
 
 
+def evaluate(tasks, solver):
+    result = []
+    predictions = []
+    for i, task in enumerate(tqdm(tasks)):
+
+        if input_output_shape_is_same(task):
+            task_result = solver.train(task['train'])
+            if task_result:
+                pred = solver.predict(task['test'])
+                score = calc_score(task['test'], pred)
+            else:
+                pred = [el['input'] for el in task['test']]
+                score = [0] * len(task['test'])
+        else:
+            pred = [el['input'] for el in task['test']]
+            score = [0] * len(task['test'])
+
+        predictions.append(pred)
+        result.append(score)
+
+    return result, predictions
+
+
 if __name__ == '__main__':
+
+    # task_solver = TaskSolverConv1(logger)
+    task_solver = TaskSolverTree(logger)
+
     train_tasks = load_data(BASE_PATH + '/training')
     evaluation_tasks = load_data(BASE_PATH + '/evaluation')
     test_tasks = load_data(BASE_PATH + '/test')
 
-    train_result, train_predictions = evaluate(train_tasks, logger)
+    train_result, train_predictions = evaluate(train_tasks, task_solver)
     train_solved = [any(score) for score in train_result]
 
     total = sum([len(score) for score in train_result])
     logger.info(f"train solved : {sum(train_solved)} from {total} ({sum(train_solved) / total})")
 
-    evaluation_result, evaluation_predictions = evaluate(evaluation_tasks, logger)
+    evaluation_result, evaluation_predictions = evaluate(evaluation_tasks, task_solver)
     evaluation_solved = [any(score) for score in evaluation_result]
 
     if DEBUG:
@@ -58,7 +88,7 @@ if __name__ == '__main__':
     total = sum([len(score) for score in evaluation_result])
     logger.info(f"evaluation solved : {sum(evaluation_solved)} from {total} ({sum(evaluation_solved) / total})")
 
-    submission = make_prediction(test_tasks, logger)
+    submission = make_prediction(test_tasks, task_solver)
     submission = submission.reset_index()
     submission.columns = ['output_id', 'output']
     submission.to_csv('submission.csv', index=False)
