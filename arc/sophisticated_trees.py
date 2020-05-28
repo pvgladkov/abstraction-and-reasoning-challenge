5cd72e12aaabf45d266baa980d28d519c631d87c
Pavel Gladkov <gladkov.p@gmail.com>
2020-05-28 10:39:43 +0300

    net

--- arc/sophisticated_trees.py
+++ arc/sophisticated_trees.py
@@ -1,8 +1,9 @@
-from arc.utils import TaskSolver
-from arc.nn import TaskSolverUNet, TaskSolverConv1
-from arc.trees import TaskSolverTree
 import numpy as np
 from xgboost import XGBClassifier
+
+from arc.nn import TaskSolverUNet
+from arc.trees import TaskSolverTree
+from arc.utils import TaskSolver
 
 
 class StackedTaskSolver(TaskSolver):
@@ -19,7 +20,7 @@ class StackedTaskSolver(TaskSolver):
         if not net_is_trained:
             return False
 
-        task_train_net = self.net.predict(task_train[1:])
+        task_train_net = self.net.predict_proba(task_train[1:])
 
         feat, target = [], []
         for orig, transformed in zip(task_train[1:], task_train_net):
@@ -29,11 +30,16 @@ class StackedTaskSolver(TaskSolver):
                 return False
 
             orig_f = self.tree.make_features(np.array(orig['input']))
-            # transformed_f = self.tree.make_features(np.array(transformed))
 
             target_f = np.array(np.array(orig['output'])).reshape(-1, )
+            full_f = np.zeros((nrows*ncols, orig_f.shape[1] + 1))
+            idx = 0
+            for i in range(nrows):
+                for j in range(ncols):
+                    full_f[idx, 0:orig_f.shape[1]] = orig_f[idx]
+                    full_f[idx, orig_f.shape[1]] = transformed[:, i, j].argmax(0) > 0
+                    idx += 1
 
-            full_f = np.hstack((orig_f, np.array(transformed).reshape(orig_f.shape[0], 1)))
             full_target = target_f
 
             feat.extend(full_f)
@@ -45,17 +51,23 @@ class StackedTaskSolver(TaskSolver):
 
     def predict(self, task_test):
 
-        task_train_net = self.net.predict(task_test)
+        task_train_net = self.net.predict_proba(task_test)
 
         predictions = []
 
         for task_num in range(len(task_test)):
-            nrows, ncols = len(task_train_net[task_num]), len(task_train_net[task_num][0])
+            nrows, ncols = len(task_test[task_num]['input']), len(task_test[task_num]['input'][0])
 
             orig_f = self.tree.make_features(np.array(task_test[task_num]['input']))
-            # transformed_f = self.tree.make_features(np.array(task_train_net[task_num]))
 
-            full_f = np.hstack((orig_f, np.array(task_train_net[task_num]).reshape(orig_f.shape[0], 1)))
+            full_f = np.zeros((nrows * ncols, orig_f.shape[1] + 1))
+
+            idx = 0
+            for i in range(nrows):
+                for j in range(ncols):
+                    full_f[idx, 0:orig_f.shape[1]] = orig_f[idx]
+                    full_f[idx, orig_f.shape[1]] = task_train_net[task_num][:, i, j].argmax(0) > 0
+                    idx += 1
 
             preds = self.xgb.predict(full_f).reshape(nrows, ncols)
             preds = preds.astype(int).tolist()
