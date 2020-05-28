39921b1b29e360b62d494df6d1d75823eba17489
Pavel Gladkov <gladkov.p@gmail.com>
2020-05-28 10:39:30 +0300

    net

--- arc/unet.py
+++ arc/unet.py
@@ -2,6 +2,10 @@ from torch import nn
 import torch
 from torch.nn import functional as F
 
+"""
+https://github.com/milesial/Pytorch-UNet
+"""
+
 
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
@@ -82,26 +86,19 @@ class UNet(nn.Module):
         self.n_classes = n_classes
         self.bilinear = bilinear
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
         factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+
+        self.inc = DoubleConv(n_channels, 10)
+        self.down1 = Down(10, 20)
+        self.down2 = Down(20, 40 // factor)
+        self.up3 = Up(40, 20 // factor, bilinear)
+        self.up4 = Up(20, 10, bilinear)
+        self.outc = OutConv(10, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x = self.down2(x2)
-        # x4 = self.down3(x3)
-        # x5 = self.down4(x4)
-        # x = self.up1(x5, x4)
-        # x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         logits = self.outc(x)
